7f377daab72fc45555a0ca826287236973989a98
nikhilkawadkar <nikhil.kawadkar9860@gmail.com>
2022-05-11 15:10:48 +0530

    Initial commit

---
+++ src/main/java/com/example/spg/controller/Contrller.java
@@ -0,0 +1,14 @@
+package com.example.spg.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Contrller {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello jenkins";
+	}
+	
+}
